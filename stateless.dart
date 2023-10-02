6c6f355df9c05038309a77179cdb8b519ce4fabf
AldiyarK <117152243+AldiyarK@users.noreply.github.com>
2023-10-02 14:15:31 +0600

    Add files via upload

---
+++ stateless.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+//Stateless Widger (Section 2)
+void main() {
+  runApp(ScreenWidget());
+}
+
+class ScreenWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('StatelessWidget'),
+        ),
+        body: SimpleWidget(),
+      ),
+    );
+  }
+}
+
+class SimpleWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('Мой текст', textDirection: TextDirection.ltr),
+      ),
+    );
+  }
+}
